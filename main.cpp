f483f8feb4b455451a58f382682245ef4f53bc9a
Jack Bonnell <jack.b@sundance.com>
2021-10-15 14:32:37 +0100

    inital commit

--- main.cpp
+++ main.cpp
@@ -9,16 +9,14 @@
 using namespace cv;
 int main(int argc, char** argv)
 {
-    VideoCapture cap;
+    VideoCapture cap(0);
     // open the default camera, use something different from 0 otherwise;
     // Check VideoCapture documentation.
        auto network = vitis::ai::FaceDetect::create(
                   "densebox_320_320",
                   true);
-            
-    if(!cap.open(0))
-        return 0;
-    for(;;)
+        
+    while(true)
     {
           Mat frame;
           cap >> frame;
@@ -27,7 +25,7 @@ int main(int argc, char** argv)
           //The VITIS-AI MAGIC
 	      auto face_results = network->run(frame);
           //Drawing boxes around results
-	      for (const auto &r : face_results.rects) {
+	    for (const auto &r : face_results.rects) {
             int x1 = r.x * frame.cols;
             int y1 = r.y * frame.rows;
             int x2 = x1 + (r.width * frame.cols);
@@ -36,12 +34,12 @@ int main(int argc, char** argv)
           // and its bottom right corner.
           Point pt2(x2, y2);
           cv::rectangle(frame, pt1, pt2, cv::Scalar(0, 255, 0));
-
+        }
 
 
           imshow("this is you, smile! :)", frame);
           if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
-    }
+         
     // the camera will be closed automatically upon exit
     // cap.close();
     return 0;
